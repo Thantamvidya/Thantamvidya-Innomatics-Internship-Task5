69965e484c0a3a546d4f2437348d164c8fac6852
Thantamvidya <tthantamvidya@gmail.com>
2025-03-11 14:18:18 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import  React from 'react'
+
+import TodoList from "./Components/TodoList"
+
+const App = () => {
+  return (
+    <>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
