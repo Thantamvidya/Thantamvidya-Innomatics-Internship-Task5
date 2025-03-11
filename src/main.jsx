f3bcab6247f9b3a4e1edb62dd08b985d86900aa9
Thantamvidya <tthantamvidya@gmail.com>
2025-03-11 14:02:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-import { createRoot } from 'react-dom/client'
-import React from 'react'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Thantamvidya-Innomatics-Internship-Task5/">
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
